19d8117ac3a3e096377a21d7bca20a20b0605410
etiennezastko <etiennezastko@hotmail.fr>
2021-04-21 09:43:21 +0200

    porté-variabless

---
+++ main.js
@@ -0,0 +1,18 @@
+const coucou = 55;
+console.log(coucou);
+
+
+
+function test(){
+    let hello = 24;
+    console.log(coucou);
+}
+document.getElementById('button').addEventListener('click',test);
+let hello = 'salut nicolas aka le boss';
+console.log(hello);
+
+function truc(){
+    
+    console.log(hello);
+}
+document.getElementById('click').addEventListener('click',truc);
